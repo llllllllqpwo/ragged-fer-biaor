861e8017685e2e5d1a747b71d8b481cb4b0546d3
llllllllqpwo <lu-hua@outlook.com>
2022-07-01 21:45:01 +0800

    Update teacher_record.py
    finish all about GUI of teacher record

--- teacher_record.py
+++ teacher_record.py
@@ -1,18 +1,20 @@
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QTableWidget, QCheckBox, QComboBox
 
 class Demo(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.exam_label = QLabel('choice exam', self)
-        self.subj_label = QLabel('choice subject', self)
-        self.show_class_check = QCheckBox('show class', self)
-        self.show_rank_check = QCheckBox('show rank', self)
+        self.exam_label = QLabel('choice exam')
+        self.subj_label = QLabel('choice subject')
+        self.show_class_check = QCheckBox('show class')
+        self.show_rank_check = QCheckBox('show rank')
         #these two lines will be enough? in chap.7
         self.exam_combo = QComboBox(self)
         self.subj_combo = QComboBox(self)
-        self.search_button = QPushButton('Start', self)
+        self.save_button = QPushButton('Save')
+        self.delete_button = QPushButton('Delete')
 
         self.h_layout = QHBoxLayout()
         self.h_layout.addWidget(self.exam_label)
@@ -21,9 +23,12 @@ class Demo(QWidget):
         self.h_layout.addWidget(self.subj_combo)
         self.h_layout.addWidget(self.show_class_check)
         self.h_layout.addWidget(self.show_rank_check)
-        self.h_layout.addWidget(self.search_button)
+        self.h_layout.addWidget(self.delete_button)
+        self.h_layout.addWidget(self.save_button)
 
         self.table = Demo_table()
+        self.delete_button.clicked.connect(self.table.dlt)
+        self.save_button.clicked.connect(lambda:print('saved'))
         self.v_layout = QVBoxLayout()
         self.v_layout.addLayout(self.h_layout)
         self.v_layout.addWidget(self.table)
@@ -37,12 +42,12 @@ class Demo_table(QTableWidget):
         super().__init__()
         self.rct = 7
         self.setRowCount(1)
-        self.setColumnCount(6)
+        self.setColumnCount(5)
         self.setColumnWidth(0, 30)
         self.setRowHeight(0, 30)
         for i in range(self.rct - 1):
             self.new_line()
-        self.setHorizontalHeaderLabels(['delete', 'name', 'code', 'k', 'd', 'a'])
+        self.setHorizontalHeaderLabels(['name', 'code', 'k', 'd', 'a'])
         self.nbuton = QPushButton('new line')
         self.nbuton.clicked.connect(self.new_line)
         self.setSpan(self.rowCount()-1, 0, 1, self.columnCount())
@@ -52,16 +57,23 @@ class Demo_table(QTableWidget):
         # self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
     def new_line(self):
         self.insertRow(self.rowCount()-1)
-        self.dbuton = QPushButton('d')
-        self.dbuton.clicked.connect(lambda:self.dlt())
-        self.setCellWidget(self.rowCount()-2, 0, self.dbuton)
+        # self.check = QTableWidgetItem()
+        # self.check.setCheckState(Qt.Unchecked)
+        # self.setItem(self.rowCount()-2, 0, self.check)
+        # self.dbuton = QPushButton('d')
+        # self.dbuton.clicked.connect(lambda:self.dlt())
+        # self.setCellWidget(self.rowCount()-2, 0, self.dbuton)
         
     def dlt(self):
-       button = self.sender()
-       if button:
-            row = self.indexAt(button.pos()).row()
-            self.removeRow(row)
-
+        s_items = self.selectedIndexes()
+        if s_items:
+            selected_rows = []
+            for i in s_items:
+                row = i.row()
+                if row not in selected_rows and row != self.rowCount()-1:
+                    selected_rows.append(row)
+            for r in range(len(sorted(selected_rows))):
+                    self.removeRow(selected_rows[r]-r)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
